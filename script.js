2ea10c39490a3a32f2ab9d506740904d3dd15789
ahsduuyro <dhineshkumarcse070@gmail.com>
2026-06-05 19:03:24 +0530

    Add files via upload
    New UI2

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-console.log("Portfolio loaded successfully 🚀");
+const roles = [
+    "Associate Team Lead",
+    "Front-End Developer",
+    "Application Support Engineer",
+    "Problem Solver",
+    "Tech Enthusiast"
+];
+
+let roleIndex = 0;
+let charIndex = 0;
+let currentRole = "";
+let isDeleting = false;
+
+const typingElement = document.getElementById("typing");
+
+function typeEffect() {
+
+    currentRole = roles[roleIndex];
+
+    if(!isDeleting){
+        typingElement.textContent =
+        currentRole.substring(0,charIndex+1);
+
+        charIndex++;
+
+        if(charIndex === currentRole.length){
+            isDeleting = true;
+            setTimeout(typeEffect,1500);
+            return;
+        }
+    }
+    else{
+        typingElement.textContent =
+        currentRole.substring(0,charIndex-1);
+
+        charIndex--;
+
+        if(charIndex === 0){
+            isDeleting = false;
+            roleIndex++;
+
+            if(roleIndex === roles.length){
+                roleIndex = 0;
+            }
+        }
+    }
+
+    setTimeout(typeEffect,
+        isDeleting ? 50 : 100);
+}
+
+typeEffect();
